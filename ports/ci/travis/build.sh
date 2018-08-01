6aa10a441ced32ab900c9da57128663767d9cd1e
Gonzalo Exequiel Pedone <hipersayan.x@gmail.com>
2018-08-01 13:11:00 -0300

    Fix build 3.

--- ports/ci/travis/build.sh
+++ ports/ci/travis/build.sh
@@ -8,6 +8,10 @@ echo "Available headers:"
 ls /usr/src | sort
 echo
 
+pushd /usr/src/linux-headers-4.4.0-131
+make oldconfig && make prepare
+popd
+
 cd src
 make KERNEL_DIR=/usr/src/linux-headers-4.4.0-131
 EOF
--- ports/ci/travis/install_deps.sh
+++ ports/ci/travis/install_deps.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+KERNEL_VERSION_A=4.17.11
+KERNEL_VERSION_B=041711
+KERNEL_VERSION_C=201807280505
+
 # Set default Docker command
 EXEC="docker exec ${DOCKERSYS}"
 ${EXEC} apt-get -y update
@@ -10,3 +14,14 @@ ${EXEC} apt-get -y install \
     g++ \
     make \
     linux-headers-generic
+
+KERNEL_VERSION=${KERNEL_VERSION_A}-${KERNEL_VERSION_B}
+url=http://kernel.ubuntu.com/~kernel-ppa/mainline/v${KERNEL_VERSION_A}
+headers=linux-headers-${KERNEL_VERSION}_${KERNEL_VERSION}.${KERNEL_VERSION_C}_all.deb
+headers_generic=linux-headers-${KERNEL_VERSION}-generic_${KERNEL_VERSION}.${KERNEL_VERSION_C}_amd64.deb
+headers_modules=linux-modules-${KERNEL_VERSION}-generic_${KERNEL_VERSION}.${KERNEL_VERSION_C}_amd64.deb
+
+for package in headers headers_generic headers_modules; do
+    ${EXEC} wget -c "${url}/${package}"
+    ${EXEC} dpkg -i "${package}"
+done
