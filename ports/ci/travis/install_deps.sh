a701f228fe818406fd3906f1b643abb19742b53a
Gonzalo Exequiel Pedone <hipersayan.x@gmail.com>
2018-08-01 14:02:50 -0300

    Fix build 8.

--- ports/ci/travis/install_deps.sh
+++ ports/ci/travis/install_deps.sh
@@ -14,7 +14,7 @@ ${EXEC} apt-get -y install \
 url=http://kernel.ubuntu.com/~kernel-ppa/mainline/v${KERNEL_VERSION_A}
 headers=linux-headers-${KERNEL_VERSION}_${KERNEL_VERSION}.${KERNEL_VERSION_C}_all.deb
 headers_generic=linux-headers-${KERNEL_VERSION}-generic_${KERNEL_VERSION}.${KERNEL_VERSION_C}_amd64.deb
-headers_modules=linux-modules-${KERNEL_VERSION}-generic_${KERNEL_VERSION}.${KERNEL_VERSION_C}_amd64.deb
+#headers_modules=linux-modules-${KERNEL_VERSION}-generic_${KERNEL_VERSION}.${KERNEL_VERSION_C}_amd64.deb
 
 for package in ${headers} ${headers_generic} ${headers_modules}; do
     ${EXEC} wget -c "${url}/${package}"
