aac63c70f8b27728d11f96a112f346623da30745
Gonzalo Exequiel Pedone <hipersayan.x@gmail.com>
2018-08-01 12:32:30 -0300

    Configurable kernel directory.

--- ports/ci/travis/build.sh
+++ ports/ci/travis/build.sh
@@ -4,7 +4,8 @@ EXEC="docker exec ${DOCKERSYS}"
 BUILDSCRIPT=dockerbuild.sh
 
 cat << EOF >> ${BUILDSCRIPT}
+ls /usr/src | sort
 cd src
-make
+make KERNEL_DIR=/usr/src/linux-headers-4.4.0-130
 EOF
 ${EXEC} bash ${BUILDSCRIPT}
