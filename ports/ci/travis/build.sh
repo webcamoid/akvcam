deaf0211ec69fc96741b1dbf3afaa88b898afe0b
Gonzalo Exequiel Pedone <hipersayan.x@gmail.com>
2018-08-01 12:39:20 -0300

    Fix build.

--- ports/ci/travis/build.sh
+++ ports/ci/travis/build.sh
@@ -4,8 +4,11 @@ EXEC="docker exec ${DOCKERSYS}"
 BUILDSCRIPT=dockerbuild.sh
 
 cat << EOF >> ${BUILDSCRIPT}
+echo "Available headers:"
 ls /usr/src | sort
+echo
+
 cd src
-make KERNEL_DIR=/usr/src/linux-headers-4.4.0-130
+make KERNEL_DIR=/usr/src/linux-headers-4.4.0-131
 EOF
 ${EXEC} bash ${BUILDSCRIPT}
