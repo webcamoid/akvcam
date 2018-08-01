d48d55c29c761958ca6d931a32315d82dc7c29a7
Gonzalo Exequiel Pedone <hipersayan.x@gmail.com>
2018-08-01 13:29:05 -0300

    Fix build 6.

--- ports/ci/travis/build.sh
+++ ports/ci/travis/build.sh
@@ -8,11 +8,11 @@ echo "Available headers:"
 ls /usr/src | sort
 echo
 
-pushd /usr/src/linux-headers-4.4.0-131
-make oldconfig && make prepare
-popd
+# pushd /usr/src/linux-headers-4.4.0-131
+# make oldconfig && make prepare
+# popd
 
 cd src
-make KERNEL_DIR=/usr/src/linux-headers-4.4.0-131
+make KERNEL_DIR=/usr/src/linux-headers-4.4.0-131-generic
 EOF
 ${EXEC} bash ${BUILDSCRIPT}
