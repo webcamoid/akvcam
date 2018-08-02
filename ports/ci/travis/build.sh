c5c05d6a2b584755f136959eb658e3b9eb93818f
Gonzalo Exequiel Pedone <hipersayan.x@gmail.com>
2018-08-02 20:28:11 -0300

    Autologin into QEMU and test the driver 10.

--- ports/ci/travis/build.sh
+++ ports/ci/travis/build.sh
@@ -46,6 +46,9 @@ sed -i 's/#NAutoVTs=6/NAutoVTs=1/' ${system_mount_point}/etc/systemd/logind.conf
 cat ${system_mount_point}/etc/systemd/logind.conf
 
 sed -i 's/\/sbin\/agetty/\/sbin\/agetty --autologin root/' ${system_mount_point}/lib/systemd/system/*getty*.service
+
+cat ${system_mount_point}/lib/systemd/system/*getty*
+
 sed -i 's/root:.:/root::/' ${system_mount_point}/etc/shadow
 mkdir -p ${system_mount_point}/etc/systemd/system/getty@\${default_terminal}.service.d
 echo '[Service]' >> ${system_mount_point}/etc/systemd/system/getty@\${default_terminal}.service.d/override.conf
