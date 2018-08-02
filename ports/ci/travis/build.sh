169b009f3ce980c061ed9700298ba706611a49bb
Gonzalo Exequiel Pedone <hipersayan.x@gmail.com>
2018-08-02 17:18:24 -0300

    Autologin into QEMU and test the driver 4.

--- ports/ci/travis/build.sh
+++ ports/ci/travis/build.sh
@@ -44,6 +44,8 @@ sed -i 's/#NAutoVTs=6/NAutoVTs=1/' ${system_mount_point}/etc/systemd/logind.conf
 sed -i 's/\/sbin\/agetty/\/sbin\/agetty -a root/' ${system_mount_point}/lib/systemd/system/getty@.service
 sed -i 's/root:.:/root::/' ${system_mount_point}/etc/shadow
 
+cat ${system_mount_point}/etc/shadow
+
 # Prepare the system to test the driver
 cp -vf src/${DRIVER_FILE} ${system_mount_point}/root
 echo './driver_test.sh' >> ${system_mount_point}/root/.profile
