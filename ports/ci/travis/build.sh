072ae7e1149aad3de266f824446740399d3ff379
Gonzalo Exequiel Pedone <hipersayan.x@gmail.com>
2018-08-22 17:00:43 -0300

    Force mkfs.

--- ports/ci/travis/build.sh
+++ ports/ci/travis/build.sh
@@ -40,7 +40,7 @@ echo
 if [ ! -z "${USE_QEMU}" ]; then
     # Create the system image to boot with QEMU.
     qemu-img create -f raw ${system_image} 1g
-    mkfs.ext4 ${system_image}
+    mkfs.ext4 -F ${system_image}
 
     # Install bootstrap system
     mkdir ${system_mount_point}
