868621e42cfd6f960169d2e3b521402d302cb529
Gonzalo Exequiel Pedone <hipersayan.x@gmail.com>
2024-06-27 16:25:45 -0300

    Fix ARM build 4.

--- ports/ci/linux/install_deps.sh
+++ ports/ci/linux/install_deps.sh
@@ -146,7 +146,7 @@ case "$architecture" in
 esac
 
 url=https://kernel.ubuntu.com/mainline/${REPOSITORY}
-headers=${systemArch}/linux-headers-${KERNEL_VERSION}_${KERNEL_VERSION}.${KERNEL_VERSION_C}_all.deb
+headers=amd64/linux-headers-${KERNEL_VERSION}_${KERNEL_VERSION}.${KERNEL_VERSION_C}_all.deb
 headers_generic=${systemArch}/linux-headers-${KERNEL_VERSION}-generic_${KERNEL_VERSION}.${KERNEL_VERSION_C}_${systemArch}.deb
 
 for package in ${headers} ${headers_generic}; do
